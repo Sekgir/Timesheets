598bec0c3888ea1d4ff18e335eab81b660458a3f
Sergey <Sekgir@mail.ru>
2021-08-10 15:34:03 +0300

    Создан проект Timesheets. Добавлены сущности Employee, Contract, Invoice, Client. Созданы контролеры, которые взаимодействуют с данными сущностями.

---
+++ Timesheets/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheets.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public double Salary { get; set; } //Почасовая оплата
+    }
+}
